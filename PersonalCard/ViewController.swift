f8deb7fd089bc3abc6820e8da6d2e6ec599463cf
Anzhelika Mihalchenia <anzhelichka1990@gmail.com>
2024-01-01 20:56:22 +0300

    Second and third screens titles

--- PersonalCard/ViewController.swift
+++ PersonalCard/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var persInfoButton: UIButton!
+    @IBOutlet weak var educationButton: UIButton!
+    @IBOutlet weak var familyButton: UIButton!
+    @IBOutlet weak var experienceButton: UIButton!
+    
+    @IBAction private func persInfoButtonDidTap() {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let secondVC = mainStoryboard.instantiateViewController(withIdentifier: "Personal InfoVC") as! ViewController
+                navigationController!.pushViewController(secondVC, animated: true)
+    }
+    
+    @IBAction private func educationButtonDidTap() {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let thirdVC = mainStoryboard.instantiateViewController(withIdentifier: "EducationVC") as! ViewController
+                navigationController!.pushViewController(thirdVC, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
