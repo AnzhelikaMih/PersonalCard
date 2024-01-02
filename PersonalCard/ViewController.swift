378a80c2e452805d34e105e745ade2a8a162377a
Anzhelika Mihalchenia <anzhelichka1990@gmail.com>
2024-01-02 19:05:58 +0300

    Third and Fourh screens, social networks buttons

--- PersonalCard/ViewController.swift
+++ PersonalCard/ViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var persInfoButton: UIButton!
     @IBOutlet weak var educationButton: UIButton!
     @IBOutlet weak var familyButton: UIButton!
     @IBOutlet weak var experienceButton: UIButton!
+    @IBOutlet weak var instagramButton: UIButton!
+    @IBOutlet weak var telegramButton: UIButton!
+    @IBOutlet weak var youtubeButton: UIButton!
     
     @IBAction private func persInfoButtonDidTap() {
         let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
@@ -25,11 +30,50 @@ class ViewController: UIViewController {
         let thirdVC = mainStoryboard.instantiateViewController(withIdentifier: "EducationVC") as! ViewController
                 navigationController!.pushViewController(thirdVC, animated: true)
     }
+    
+    @IBAction private func familyButtonDidTap() {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let fourthVC = mainStoryboard.instantiateViewController(withIdentifier: "FamilyVC") as! ViewController
+                navigationController!.pushViewController(fourthVC, animated: true)
+    }
+    
+    @IBAction private func experienceButtonDidTap() {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let fifthVC = mainStoryboard.instantiateViewController(withIdentifier: "ExperienceVC") as! ViewController
+                navigationController!.pushViewController(fifthVC, animated: true)
+    }
+    
+    @IBAction func instagramButtonDidTap() {
+        let url = URL(string: "https://www.instagram.com/anzhelika_mihalchenia?igsh=YTQwZjQ0NmI0OA%3D%3D&utm_source=qr")!
+        UIApplication.shared.open(url, options: [:])
+    }
+    
+    @IBAction func telegramButtonDidTap() {
+        let url = URL(string: "https://t.me/talkToLika")!
+        UIApplication.shared.open(url, options: [:])
+    }
+    
+    @IBAction func youtubeButtonDidTap() {
+        let url = URL(string: "https://www.youtube.com/@DM-xe4gu")!
+        UIApplication.shared.open(url, options: [:])
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func privateFunc() {
+    }
 
-
+    private func privateMethod() {
+    }
 }
 
+extension ViewController: SomeProtocol {
+}
+
+extension ViewController {
+}
+
+protocol SomeProtocol {
+}
